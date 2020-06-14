b6d86749dfb9f3384af97f19451ec8b82c412678
snhrknsk <snhrknsk@gmail.com>
2020-06-14 19:37:11 +0900

    fix test case

--- jpl/test/ch22/ex15/CalculatorTest.java
+++ jpl/test/ch22/ex15/CalculatorTest.java
@@ -1,14 +1,17 @@
 package ch22.ex15;
 
+import static org.junit.Assert.*;
+
 import org.junit.Test;
 
 public class CalculatorTest {
 
 	@Test
 	public void test() {
-		String source = "9 1 + 2 - 4 / 3 %";//=2
+		String source = "9 1 + 2 - 4 / 3 % 2 + sqrt";//sqrt(9+1-2/4%3)=2
+		double expected = 2;
 		double result = Calculator.calc(source);
-		System.out.println(result);
+		assertEquals(expected, result, 0);
 	}
 
 }
