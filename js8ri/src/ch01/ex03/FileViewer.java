c5215b9ad33a1d969f57bcdc441d9dd7896d49fc
snhrknsk <snhrknsk+git@gmail.com>
2020-10-14 20:36:20 +0900

    fix ex03/FileViewer.java

--- js8ri/src/ch01/ex03/FileViewer.java
+++ js8ri/src/ch01/ex03/FileViewer.java
@@ -7,17 +7,11 @@ import java.util.List;
 
 public class FileViewer {
 
-	private static final String directoryPath = "C:\\GitFolder\\java-learning\\js8ri\\src\\ch01\\ex03";
-
 	public static List<String> getAllFile(File file, String extension) {
 		List<String> result = new ArrayList<>();
+		// extensionがエンクロージングからキャプチャされる変数(ラムダ式内で定義していない)
+		// extensionは実質final,ここでextension = ".jpeg";を記載したらエラー
 		result.addAll(Arrays.asList(file.list((f, s)->s.endsWith(extension))));
 		return result;
-	}
-
-	public static void main(String[] args) {
-
-		List<String> result = getAllFile(new File(directoryPath), ".java");
-		System.out.println(result);
 	}
 }
