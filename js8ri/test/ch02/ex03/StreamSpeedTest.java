a0e58b8aae747d2366cee2fb9d5454f19766d98b
snhrknsk <snhrknsk+git@gmail.com>
2020-10-16 01:11:15 +0900

    Fix test code

--- js8ri/test/ch02/ex03/StreamSpeedTest.java
+++ js8ri/test/ch02/ex03/StreamSpeedTest.java
@@ -3,27 +3,26 @@ package ch02.ex03;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
+import java.util.regex.Pattern;
 
 import org.junit.jupiter.api.Test;
 
 class StreamSpeedTest {
 
-	private static String path = "./test/ch02/ex03/LongText.txt";// "./LongText.txt";
+	private static String path = "./LongText.txt";
 	// "./test/ch02/ex03/LongText.txt" //Eclipse上でのテスト実行パス
 
 	@Test
 	void test() {
 
 		try {
-			List<String> words = Arrays.asList(new String(Files.readAllBytes(Paths.get(path))).split("[\\P{L}]+"));
+			String words = new String(Files.readAllBytes(Paths.get(path)));
 			// Stream利用
-			long streamResult = StreamSpeed.measureWordCountSpeed(words.stream());
+			long streamResult = StreamSpeed.measureWordCountSpeed(Pattern.compile("[\\P{L}]+").splitAsStream(words));
 			System.out.println("Sequential:\t" + streamResult + "[ns]" );
 
 			// Parallel Stream利用
-			long parallelStreamResult = StreamSpeed.measureWordCountSpeed(words.parallelStream());
+			long parallelStreamResult = StreamSpeed.measureWordCountSpeed(Pattern.compile("[\\P{L}]+").splitAsStream(words));
 			System.out.println("Parallel:\t" + parallelStreamResult + "[ns]" );
 		} catch (IOException e) {
 			e.printStackTrace();
